db3f940b6f7ba2cfdf6f0b8404cc2c5f79ff6b56
terron23 <tmj72386@gmail.com>
2019-01-24 20:41:49 -0500

    Studio on Demand

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 authRoutes(app);
 studioOwners(app);
 
-app.get('*', (req, res) =>{
+app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'index.html'));
   });
 
